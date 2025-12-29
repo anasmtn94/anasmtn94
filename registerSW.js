68f9e149132c5727241f0ce4862258f778c02610
anasmtn94 <anasmtn94@gmail.com>
2025-12-29 18:44:14 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/anasmtn94/sw.js', { scope: '/anasmtn94/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
